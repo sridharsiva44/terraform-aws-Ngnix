25e793e61526b1eeef4ea25db0c445c22cc7fc6d
sridharsiva44 <r.s.sridharvenkat@gmail.com>
2025-02-12 21:14:14 +0530

    Add Public and private Subnet

--- main.tf
+++ main.tf
@@ -5,3 +5,16 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
+
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table_association" "public_associations" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
--- variables.tf
+++ variables.tf
@@ -5,3 +5,13 @@ variable "region" {
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
