52f35bcd1a3d7cb99e63a88822431f620ef95bc8
sridharsiva44 <r.s.sridharvenkat@gmail.com>
2025-02-12 21:16:08 +0530

    Add Internet gatway

--- main.tf
+++ main.tf
@@ -18,3 +18,17 @@ resource "aws_route_table_association" "public_associations" {
   subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
